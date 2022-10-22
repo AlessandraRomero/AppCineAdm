4f7235485e022e71ac8cdeead451ab91fb1780ae
AlessandraRomero <alessandraromeiro098@gmail.com>
2022-10-21 22:40:05 -0300

    first commit

---
+++ src/Pages/AdicionarCategoria.tsx
@@ -0,0 +1,9 @@
+import { HeaderApp } from "../components/HeaderApp";
+
+export function AdicionarCategoria(){
+    return (
+        <div className="adicionarCategoria">
+            <HeaderApp/>
+        </div>
+    )
+}
